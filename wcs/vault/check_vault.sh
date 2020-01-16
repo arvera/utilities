ff0e31fa3fd6cdc1838e17e517836cd37109f678
Angel Vera <angel.vera@hcl.com>
2020-01-15 22:38:48 -0500

    Enhanced for two checks

--- wcs/vault/check_vault.sh
+++ wcs/vault/check_vault.sh
@@ -16,4 +16,9 @@ vault_token=$1
 master_node_ip=$2
 
 # The cmd to run
-curl -X GET -H "X-Vault-Token:${vault_token}" http://${master_node_ip}:30552/v1/demo/qa/domainName"
+echo "Checking consul for: http://${master_node_ip}:30552/v1/demo/qa/domainName "
+curl -X GET -H "X-Vault-Token:${vault_token}" http://${master_node_ip}:30552/v1/demo/qa/domainName
+
+echo ""
+echo "Checking consul for: http://${master_node_ip}:30552/v1/demo/qa/auth/dbName"
+curl -X GET -H "X-Vault-Token:${vault_token}" http://${master_node_ip}:30552/v1/demo/qa/auth/dbName
